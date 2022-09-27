cae1c3a5ec1eb7e53e27f4df6416695845484a0d
GunDumMan <55555topkung@gmail.com>
2022-09-27 12:50:53 +0700

    AddData-getAll-getID-getCategory

--- routes/products.js
+++ routes/products.js
@@ -7,4 +7,42 @@ router.get('/', (req, res) => {
     res.json(data);
 });
 
+
+router.get('/:id', (req, res) => {
+    const productId = Number.parseInt(req.params.id);
+    const product = data.find((product) => product.id === productId);
+    res.json(product);
+});
+
+router.get('/category/:categoryID', (req, res) => {
+    const productId = Number.parseInt(req.params.categoryID);
+    const product = data.filter((product) => product.category.categoryID === productId);
+    res.json(product);
+});
+
+let currentProductId = 40;
+router.post('/', (req, res) => {
+    const {
+        imgUrl, title, category, detail, currentBid, buyNow, bitAmount, countDown, owner, winnerBid
+    } = req.body;
+    const product = {
+        id: ++currentProductId,
+        imgUrl,
+        title,
+        category: {
+            categoryID: category.categoryID,
+            caregoryName: category.caregoryName,
+        },
+        detail,
+        currentBid,
+        buyNow,
+        bitAmount,
+        countDown,
+        owner,
+        winnerBid,
+    };
+    data.push(product);
+    res.json(product);
+});
+
 module.exports = router;
